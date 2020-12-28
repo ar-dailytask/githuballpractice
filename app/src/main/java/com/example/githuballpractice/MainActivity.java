8a1fd133adaf6f4eb97f618b6c89df6f9c045306
ar-dailytask <75058768+ar-dailytask@users.noreply.github.com>
2020-12-28 15:52:36 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/githuballpractice/MainActivity.java
+++ app/src/main/java/com/example/githuballpractice/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+/* the files which we have changed
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
