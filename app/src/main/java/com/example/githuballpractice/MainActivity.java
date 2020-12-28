df360529991244566e5d12d5a168dac878ffcc8f
ashutosh <ashutaskactivity@gmail.com>
2020-12-28 15:45:39 +0530

    message for the commit

--- app/src/main/java/com/example/githuballpractice/MainActivity.java
+++ app/src/main/java/com/example/githuballpractice/MainActivity.java
@@ -4,4 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity -u
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
